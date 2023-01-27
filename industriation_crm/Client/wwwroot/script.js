872cade2363fe179c3b82743bce41fddf89ac259
Maxim Kosarev <1337elmaxon@gmail.com>
2023-01-27 09:12:38 +0300

    qweqw

--- industriation_crm/Client/wwwroot/script.js
+++ industriation_crm/Client/wwwroot/script.js
@@ -19,3 +19,32 @@ function tabsSection() {
         });
     }
 };
+
+$('body').on('focus', 'input[type="text"]', function() {
+
+    var mask_option = {phone: '+{7}(000)000-00-00'};
+ 
+
+    if($(this).hasClass('phoneMasked'))
+        setMaskI(mask_option['phone'], $(this));
+
+});
+
+function setMaskI(_mask, _this) {
+
+    _this_classes = _this[0].className.split(' ');
+    _js_query = '.' + _this_classes.join('.');
+
+    var maskOptions = {
+        mask: _mask
+    };
+    var mask = IMask(document.querySelector(_js_query), maskOptions);
+
+    //console.log('unmasked value => ', mask.unmaskedValue);
+    
+}
+
+
+
+    
+
